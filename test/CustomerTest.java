4986a5fe16870d9c493575ff2fb847f33e78a8c0
Patrick Starzynski <patrick_starzynski@web.de>
2015-06-03 17:20:07 +0200

    added test classes

---
+++ test/CustomerTest.java
@@ -0,0 +1,35 @@
+package b06.refactoring;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Patrick on 03.06.2015.
+ */
+public class CustomerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGetName() throws Exception {
+
+    }
+
+    @Test
+    public void testAddRental() throws Exception {
+
+    }
+
+    @Test
+    public void testStatement() throws Exception {
+
+    }
+
+    @Test
+    public void testHtmlStatement() throws Exception {
+
+    }
+}
---
+++ test/RentalTest.java
@@ -0,0 +1,25 @@
+package b06.refactoring;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Patrick on 03.06.2015.
+ */
+public class RentalTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGetMovie() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDaysRented() throws Exception {
+
+    }
+}
