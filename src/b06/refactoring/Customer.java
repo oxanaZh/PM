6f750fdd4b33916cd99220e44f0aa2af94a68238
Patrick Starzynski <patrick_starzynski@web.de>
2015-06-04 15:26:19 +0200

    add new tests

--- src/b06/refactoring/Customer.java
+++ src/b06/refactoring/Customer.java
@@ -21,46 +21,46 @@ public class Customer {
     }
 
     public String generateStatement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
+            double totalAmount = 0;
+            int frequentRenterPoints = 0;
 
-        String result = "Rental record for " + getName() + "\n";
-        for (Rental rental : rentalList) {
-            double amount = 0;
-            switch (rental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                amount += 2;
-                break;
-            case Movie.NEWRELEASE:
-                amount += rental.getDaysRented() * 3;
-                break;
-            case Movie.CHLDRNS:
-                amount += 1.5;
-        }
-
-        if (rental.getMovie().getPriceCode() != Movie.NEWRELEASE) {
-            if (rental.getDaysRented() > 2) {
-                if (rental.getMovie().getPriceCode() == Movie.CHLDRNS){
-                    if( rental.getDaysRented() > 3 )
-                        amount+=(rental.getDaysRented()-3)*1.5;
-                }else{
-                if( rental.getMovie().getPriceCode() == Movie.REGULAR)
-                    amount+=(rental.getDaysRented()-2)*1.5;
+            String result = "Rental record for " + getName() + "\n";
+            for (Rental rental : rentalList) {
+                double amount = 0;
+                switch (rental.getMovie().getPriceCode()) {
+                    case Movie.REGULAR:
+                        amount += 2;
+                        break;
+                    case Movie.NEWRELEASE:
+                        amount += rental.getDaysRented() * 3;
+                        break;
+                    case Movie.CHLDRNS:
+                        amount += 1.5;
                 }
-            }
-        }
 
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if (rental.getMovie().getPriceCode() == Movie.NEWRELEASE && rental.getDaysRented() > 1)
+                if (rental.getMovie().getPriceCode() != Movie.NEWRELEASE) {
+                    if (rental.getDaysRented() > 2) {
+                        if (rental.getMovie().getPriceCode() == Movie.CHLDRNS){
+                            if( rental.getDaysRented() > 3 )
+                                amount+=(rental.getDaysRented()-3)*1.5;
+                        }else{
+                            if( rental.getMovie().getPriceCode() == Movie.REGULAR)
+                                amount+=(rental.getDaysRented()-2)*1.5;
+                        }
+                    }
+                }
+
+                // add frequent renter points
                 frequentRenterPoints++;
+                // add bonus for a two day new release rental
+                if (rental.getMovie().getPriceCode() == Movie.NEWRELEASE && rental.getDaysRented() > 1)
+                    frequentRenterPoints++;
 
-            // show figures for this rental
-            result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(amount) + "\n";
+                // show figures for this rental
+                result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(amount) + "\n";
 
-            totalAmount += amount;
-        }
+                totalAmount += amount;
+            }
 
         result += "Amount owned is " + String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
--- test/b06/refactoring/CustomerTest.java
+++ test/b06/refactoring/CustomerTest.java
@@ -25,26 +25,65 @@ public class CustomerTest {
 
     @Test
     public void testAddRental() throws Exception {
-        customer.addRental(new Rental(new Movie("testMovie",5),5));
+        customer.addRental(new Rental(new Movie("testMovie",2),5));
         assertNotNull(customer.generateStatement());
 
     }
 
     @Test
-    public void testStatement() throws Exception {
-        customer.addRental(new Rental(new Movie("testMovie",5),5));
-        String outputString ="Rental record for testCustomer\n"+"\ttestMovie\t0.0\n"+ "Amount owned is 0.0\n" + "You earned 1 frequent renter points";
+    public void testStatementChildren() throws Exception {
+        customer.addRental(new Rental(new Movie("testMovie",2),5));
+        String outputString ="Rental record for testCustomer\n"+"\ttestMovie\t4.5\n"+ "Amount owned is 4.5\n" + "You earned 1 frequent renter points";
         assertEquals(outputString,customer.generateStatement());
-        assertNotEquals(outputString,customer.generateHtmlStatement());
+
+    }
+    @Test
+    public void testStatementRegular()  throws Exception {
+        customer.addRental(new Rental(new Movie("testMovie",1),5));
+        String outputString ="Rental record for testCustomer\n"+"\ttestMovie\t15.0\n"+ "Amount owned is 15.0\n" + "You earned 2 frequent renter points";
+        assertEquals(outputString,customer.generateStatement());
+    }
+    @Test
+    public void testStatementNewRelease()  throws Exception {
+        customer.addRental(new Rental(new Movie("testMovie",0),5));
+        String outputString ="Rental record for testCustomer\n"+"\ttestMovie\t6.5\n"+ "Amount owned is 6.5\n" + "You earned 1 frequent renter points";
+        assertEquals(outputString,customer.generateStatement());
+    }
+    @Test
+    public void testStatementMultipleRental()  throws Exception {
+        customer.addRental(new Rental(new Movie("testMovie",2),5));
+        customer.addRental(new Rental(new Movie("testMovie",1),5));
+        customer.addRental(new Rental(new Movie("testMovie",0),5));
+        String outputString ="Rental record for testCustomer\n"+"\ttestMovie\t4.5\n"+"\ttestMovie\t15.0\n"+"\ttestMovie\t6.5\n"+ "Amount owned is 26.0\n" + "You earned 4 frequent renter points";
+        assertEquals(outputString,customer.generateStatement());
     }
 
+
+
     @Test
-    public void testHtmlStatement() throws Exception {
-        customer.addRental(new Rental(new Movie("testMovie",5),5));
-        String outputString ="<h1>Rental record for <b>testCustomer</b></h1>\n"+"<p>testMovie\t0.0</p>\n"+ "<p>Amount owned is <b>0.0</b></p>\n" + "<p>You earned <b>1 frequent renter points</b></p>";
+    public void testHtmlStatementChildren() throws Exception {
+        customer.addRental(new Rental(new Movie("testMovie",2),5));
+        String outputString ="<h1>Rental record for <b>testCustomer</b></h1>\n"+"<p>testMovie\t4.5</p>\n"+ "<p>Amount owned is <b>4.5</b></p>\n" + "<p>You earned <b>1 frequent renter points</b></p>";
         assertEquals(outputString, customer.generateHtmlStatement());
-        assertNotEquals(outputString, customer.generateStatement());
-
-
+    }
+    @Test
+    public void testHtmlStatementRegular() throws Exception {
+        customer.addRental(new Rental(new Movie("testMovie",1),5));
+        String outputString ="<h1>Rental record for <b>testCustomer</b></h1>\n"+"<p>testMovie\t15.0</p>\n"+ "<p>Amount owned is <b>15.0</b></p>\n" + "<p>You earned <b>2 frequent renter points</b></p>";
+        assertEquals(outputString, customer.generateHtmlStatement());
+    }
+    @Test
+    public void testHtmlStatementNewRelease() throws Exception {
+        customer.addRental(new Rental(new Movie("testMovie",0),5));
+        String outputString ="<h1>Rental record for <b>testCustomer</b></h1>\n"+"<p>testMovie\t6.5</p>\n"+ "<p>Amount owned is <b>6.5</b></p>\n" + "<p>You earned <b>1 frequent renter points</b></p>";
+        assertEquals(outputString, customer.generateHtmlStatement());
+    }
+    @Test
+    public void testHtmlStatementMultipleRental()  throws Exception {
+        customer.addRental(new Rental(new Movie("testMovie",2),5));
+        customer.addRental(new Rental(new Movie("testMovie",1),5));
+        customer.addRental(new Rental(new Movie("testMovie",0),5));
+        String outputString ="<h1>Rental record for <b>testCustomer</b></h1>\n"+"<p>testMovie\t4.5</p>\n"+"<p>testMovie\t15.0</p>\n"+"<p>testMovie	6.5</p>\n"+"<p>Amount owned is <b>26.0</b></p>\n" + "<p>You earned <b>4 frequent renter points</b></p>";
+        assertEquals(outputString,customer.generateHtmlStatement());
     }
 }
