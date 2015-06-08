0e26f44ebad871bf2a07eb1a3490f9ed442c3870
Carsten Gips <carsten.gips@fh-bielefeld.de>
2015-06-08 20:25:57 +0200

    Testsuite für Vorgabetests

---
+++ tests/b07/VorgabeTests.java
@@ -0,0 +1,13 @@
+package b07;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+    IteratorTest.class,
+    LinkedListTest.class,
+    SortTest.class })
+public class VorgabeTests {
+}
